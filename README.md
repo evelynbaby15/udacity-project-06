e3bc826471f6d7a1efd43d8c50b3ba10654b1440
evleyn <evelyn.b9044015@gmail.com>
2019-01-19 02:18:34 +0000

    :white_check_mark: fix testing logic error for async function.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # How to run?
 
-Clone the project and open index.html in browser.
-After page loading finished, scroll down to bottom to see Jasmine test result.
+1. Clone the project to your pc or download .zip for this project.
+2. Open index.html in your browser.
+3. After page loading finished, scroll down to the bottom and you'll see Jasmine test report.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,7 +40,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -53,15 +53,15 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
 
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+         /* Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -77,14 +77,16 @@ $(function() {
             var link = document.querySelector('.menu-icon-link');
             console.log('link:', link);
             link.click();            
+            expect(bodyCls).not.toContain('menu-hidden');
 
-            expect(bodyCls).not.toContain('menu-hidden');               
+            link.click();
+            expect(bodyCls).toContain('menu-hidden');
         })
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -97,47 +99,42 @@ $(function() {
             });            
         });
 
-        it('load feed async test', function(done) {
+        it('load feed async test', function() {
             // var container = $('.feed');
             var entrys = document.querySelectorAll('.feed .entry');
-            console.log('ent:', entrys);
+            // console.log('ent:', entrys);
 
             expect(entrys).not.toBe(null);
             expect(entrys.length).toBeGreaterThan(0);
-            done();
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
     describe('New Feed Selection', function() {
         // .feed article h2
-        var h2Content;
+        var feedContent0;
+        var feedContent1;
 
         beforeEach(function(done) {
-            loadFeed(0, done);
-            h2Content = document.querySelectorAll('.feed article h2')[0];
-            // console.log('Get h2Content:', h2Content);            
-            // done();
+            loadFeed(0, function() {
+                feedContent0 = document.querySelectorAll('.feed article h2')
+                done();
+            });
         });
 
         it('feed changes', function(done) {
-            setTimeout(function() {
-                // do some stuff
-                loadFeed(1, done);
-                // done();
-            
-              }, 1000);
-            
-            var afterLoadH2 = document.querySelectorAll('.feed article h2')[0];           
+            loadFeed(1, function() {
+                feedContent1 = document.querySelectorAll('.feed article h2')
+                done();
+            });
 
-            expect(afterLoadH2).not.toEqual(h2Content);
+            expect(feedContent0).not.toEqual(feedContent1);
         });
         
-
     });
 }());
